a3aff9b3693ec7d4e4637d9b92cdde242ab176ce
welch <>
2000-08-24 01:13:02 +0000

    Changed to version 2.1 Made code that uses Tcl 8.4 APIs conditional using #ifdef

--- generic/thread.h
+++ generic/thread.h
@@ -6,8 +6,8 @@
 
 /* remember to change win/vc/makefile.vc as well when these change */
 #define THREAD_MAJOR_VERSION  2
-#define THREAD_MINOR_VERSION  0
-#define THREAD_VERSION        "2.0"
+#define THREAD_MINOR_VERSION  1
+#define THREAD_VERSION        "2.1"
 
 #include "tcl.h"
 
