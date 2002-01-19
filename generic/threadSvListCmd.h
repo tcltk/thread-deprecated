86aac608cd5934ae82b69b67a0cb7ff11f7ae510
vasiljevic <>
2002-01-19 23:10:41 +0000

    Added as the part of upcoming 2.4. Those are implementing almost complete list command interface to thread shared variables.

---
+++ generic/threadSvListCmd.h
@@ -0,0 +1,24 @@
+/*
+ * See the file "license.txt" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ * Rcsid: @(#)$Id$
+ * ---------------------------------------------------------------------------
+ */
+
+#ifndef _SV_LIST_H_
+#define _SV_LIST_H_
+
+void Sv_RegisterListCommands();
+
+#endif /* _SV_LIST_H_ */
+
+/* EOF $RCSfile$ */
+
+/* Emacs Setup Variables */
+/* Local Variables:      */
+/* mode: C               */
+/* indent-tabs-mode: nil */
+/* c-basic-offset: 4     */
+/* End:                  */
+
